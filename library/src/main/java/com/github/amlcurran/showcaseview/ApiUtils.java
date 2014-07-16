863d5de3facb810d6928bf7f03e01bbe9a4be732
Felipe Lima <felipe.lima@gmail.com>
2014-07-16 19:20:31 -0300

    Reverts changes

--- library/src/main/java/com/github/amlcurran/showcaseview/ApiUtils.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/ApiUtils.java
@@ -23,23 +23,4 @@ public class ApiUtils {
             view.setFitsSystemWindows(true);
         }
     }
-
-    public static DisplayMetrics getDisplayMetrics(final Context context) {
-        final WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        final DisplayMetrics metrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(metrics);
-        return metrics;
-    }
-
-    public static int getScreenWidth(final Context context) {
-        if (context == null)
-            return 0;
-        return getDisplayMetrics(context).widthPixels;
-    }
-
-    public static int getScreenHeight(final Context context) {
-        if (context == null)
-            return 0;
-        return getDisplayMetrics(context).heightPixels;
-    }
 }
